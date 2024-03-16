61135028d90ba7dbf2cc0e6503e487641c6dc71d
minh12345685 <dinhminh0403@gmail.com>
2024-03-16 10:50:16 +0700

    123

--- Techboys/view/home.php
+++ Techboys/view/home.php
@@ -7,14 +7,14 @@
             <div class="container fill_height">
                 <div class="row fill_height">
                     <div class="banner_product_image">
-                        <img src="images/banner_product.png" alt="" />
+                        <img src="images/iphone.png" alt="" />
                     </div>
                     <div class="col-lg-5 offset-lg-4 fill_height">
                         <div class="banner_content">
-                            <h1 class="banner_text">new era of smartphones</h1>
-                            <div class="banner_price"><span>$530</span>$460</div>
-                            <div class="banner_product_name">Apple Iphone 6s</div>
-                            <div class="button banner_button"><a href="#">Shop Now</a></div>
+                            <h1 class="banner_text">Sản phẩm mới ra mắt</h1>
+                            <div class="banner_price"><span>$1030</span>$1000</div>
+                            <div class="banner_product_name">Iphone 15 pro max</div>
+                            <div class="button banner_button"><a href="index.php?act=cuahang">Mua ngay</a></div>
                         </div>
                     </div>
                 </div>
