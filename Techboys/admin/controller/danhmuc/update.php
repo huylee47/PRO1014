6d481d290790592805414466ee3fa14ce7eded2a
hieunt2045 <hieudao20456@gmail.com>
2024-03-19 11:27:08 +0700

    ewe

--- Techboys/admin/controller/danhmuc/update.php
+++ Techboys/admin/controller/danhmuc/update.php
@@ -1,14 +1,13 @@
 <?php
 require_once 'model/danhmuc.php';
 if (isset($_GET['id'])) {
-    $motdanhmuc =list_motdanhmuc($_GET['id']);
+    $motdanhmuc = list_motdanhmuc($_GET['id']);
     require_once 'view/danhmuc/update.php';
-}
-if(isset($_POST['capnhat'])){
+} else if (isset($_POST['capnhat'])) {
     $id = $_POST['id'];
     $tendm = $_POST['ten'];
     update_danhmuc($id, $tendm);
-    header('location: index.php?act=listdanhmuc');
+    // header('Location:index.php?act=listdanhmuc');
+    echo "<script type='text/javascript'>window.top.location='http://localhost/PRO1014/Techboys/admin/index.php?act=listdanhmuc';</script>";
+    exit;
 }
-
-?>
