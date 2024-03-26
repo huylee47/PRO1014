6240c7883ae99e8428e09bf38b16a9ca1e39e308
minh12345685 <dinhminh0403@gmail.com>
2024-03-26 15:44:18 +0700

    1

--- Techboys/admin/controller/voucher/xoa.php
+++ Techboys/admin/controller/voucher/xoa.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'model/voucher.php';
-if (isset($_GET['id'])) {
+if (isset($_GET['id'])&& $_GET['id']>0) {
     xoa_voucher($_GET['id']);
 }
 echo "<script type='text/javascript'>window.top.location='http://localhost/PRO1014/Techboys/admin/index.php?act=listvoucher';</script>";
