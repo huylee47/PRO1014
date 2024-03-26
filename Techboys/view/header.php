ad698db8909802c8643f10032693ad4207a9be14
hoc281 <hocnmpp03062@fpt.edu.vn>
2024-03-26 21:01:33 +0700

    session

--- Techboys/view/header.php
+++ Techboys/view/header.php
@@ -44,12 +44,11 @@
                             <div class="top_bar_content ml-auto">
                             <?php                    
                             if(isset($_SESSION['user'])){
-                                $username = $_SESSION['user'];
                             ?>
                                 <div class="top_bar_user">
-                                <div style="margin-right: 10px;">Hello <?php echo $username;?></div> 
+                                <div style="margin-right: 10px;">Hello <?php echo $_SESSION['user']['user']?></div> 
                                 <?php
-                                if (isset($_SESSION['quyen']) && $_SESSION['quyen'] =='1') {
+                                if (isset($_SESSION['user']['quyen']) && $_SESSION['user']['quyen'] =='1') {
                                     ?>
                                     <div><a href="http://localhost/pro1014/Techboys/admin/">ADMIN</a></div>
                                     <?php
