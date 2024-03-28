95070bdd27b0f0c25e80b1de1b837534eac5c507
minh12345685 <dinhminh0403@gmail.com>
2024-03-28 15:21:46 +0700

    123

--- Techboys/controller/dathang.php
+++ Techboys/controller/dathang.php
@@ -1,34 +1,19 @@
 <?php
 require_once './view/header.php';
 require_once "model/dathang.php";
-if (isset($_SESSION['user'])) {
-    $id_taikhoan = $_SESSION['user']['id_taikhoan'];
-    $ten = $_POST['ten'];
-    $sdt = $_POST['sdt'];
-    $ghichu = $_POST['ghichu'];
-    $diachi = $_POST['diachi'];
-    $pttt = $_POST['pttt'];
-    $idHoaDon = insert_hoadon($id_taikhoan, $ten, $sdt, $ghichu, $diachi, $pttt);
-    $sanpham = $_SESSION['giohang'];
-    foreach ($sanpham as $sp) {
-        insert_chitethoadon($idHoaDon, $sp['id_sanpham'], $sp['gia'], $sp['soluong']);
-    }
-    $_SESSION['giohang'] = [];
-}else{
-    $id_taikhoan = 0;
-    $ten = $_POST['ten'];
-    $sdt = $_POST['sdt'];
-    $ghichu = $_POST['ghichu'];
-    $diachi = $_POST['diachi'];
-    $pttt = $_POST['pttt'];
-    $idHoaDon = insert_hoadon($id_taikhoan, $ten, $sdt, $ghichu, $diachi, $pttt);
-    $sanpham = $_SESSION['giohang'];
-    foreach ($sanpham as $sp) {
-        insert_chitethoadon($idHoaDon, $sp['id_sanpham'], $sp['gia'], $sp['soluong']);
-    }
-    $_SESSION['giohang'] = [];
+require_once "model/giohang.php";
+
+$id_taikhoan = isset($_SESSION['user']) ? $_SESSION['user']['id_taikhoan'] : 0;
+$ten = $_POST['ten'];
+$sdt = $_POST['sdt'];
+$ghichu = $_POST['ghichu'];
+$diachi = $_POST['diachi'];
+$pttt = $_POST['pttt'];
+$idHoaDon = insert_hoadon($id_taikhoan, $ten, $sdt, $ghichu, $diachi, $pttt);
+$sanpham = isset($_SESSION['user']) ? layChiTietGioHang($id): $_SESSION['giohang'];
+foreach ($sanpham as $sp) {
+    insert_chitethoadon($idHoaDon, $sp['id_sanpham'], $sp['gia'], $sp['soluong']);
 }
-
-
+$_SESSION['giohang'] = [];
 
 require_once "view/giohang.php";
